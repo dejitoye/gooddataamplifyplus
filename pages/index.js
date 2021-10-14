d143b4ae54a5b02850e576be229b11ed4da668b3
dejitoye <dejitoye71@gmail.com>
2021-10-14 06:52:10 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {withAuthenticator} from "@aws-amplify/ui-react"
 import { User } from 'src/models'
-import { DataStore } from '@aws-amplify/datastore'
+import { DataStore,Predicates } from '@aws-amplify/datastore'
 import { useEffect } from 'react'
 import { listUsers } from 'src/graphql/queries'
 import { API,graphqlOperation,Auth } from "aws-amplify"
  function Home() {
   useEffect(() => {
     const ddd = async()=>{
-      const fffr = await DataStore.query(User)
+      const fffr = await DataStore.query(User,Predicates)
       const fff = await API.graphql(graphqlOperation(listUsers))
       console.log(fff)
       console.log(fffr)
